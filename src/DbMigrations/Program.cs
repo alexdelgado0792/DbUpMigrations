71a867897eebdf894c0aff2359c3579900c99930
Alex Delgado <arekkusu0792@gmail.com>
2024-08-28 15:36:08 -0600

    Update Program.cs
    test implementation

--- src/DbMigrations/Program.cs
+++ src/DbMigrations/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace DbMigrations
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DbMigrations
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int exitCode = 0;
+            try
+            {
+                var configuration = BuildConfig();
+
+                var database = configuration.GetConnectionString("ConnectionString");
+                var dbSchema = configuration.GetConnectionString("DbSchema");
+                var reportHtmlPath = configuration.GetSection("HtmlReportPath").Value;
+
+                if (string.IsNullOrEmpty(database) || string.IsNullOrEmpty(dbSchema) || string.IsNullOrEmpty(reportHtmlPath)) 
+                {
+                    throw new ArgumentException("Database configuration is not set properly. Please check Schema and Connection string.");
+                }
+
+               var migration = new Migration(database, dbSchema, reportHtmlPath);
+                migration.ShowScriptsHistory();
+                //migration.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+                exitCode = 1;
+            }
+            
+
+            return exitCode;
+        }
+
+        private static IConfiguration BuildConfig()
+        {
+
+            var builder = new ConfigurationBuilder();
+            return builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
         }
     }
 }
