56c1878cdbcaf136eb893e585d0cb0abb3e620b9
Alex Delgado <arekkusu0792@gmail.com>
2024-09-02 12:10:23 -0600

    create options enum

---
+++ src/DbMigrations/Enums/MenuOption.cs
@@ -0,0 +1,14 @@
+﻿namespace DbMigrations
+{
+    public static partial class MigrationHelper
+    {
+        public enum MenuOption 
+        {
+            None = 0,
+            DbStatus ,
+            HtmlReport,
+            Migrate,
+            Exit
+        }
+    }
+}
